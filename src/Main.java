f6a8a26548d821accec849233f80fce96c2a3140
rbabari <r.babari@gmail.com>
2023-07-03 10:25:43 -0400

    suite singleton

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,11 @@ public class Main {
         Singleton s1 = Singleton.getInstance();
         Singleton s2 = Singleton.getInstance();
 
+        if (s1 == s2)
+        {
+            System.out.println("Meme instance");
+
+        }
 
 
 
